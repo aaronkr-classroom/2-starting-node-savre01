e4157495b2c6214cb873094c4da863c769473469
savre01 <163230719+savre01@users.noreply.github.com>
2024-03-13 05:59:28 +0000

    2주차 끝

--- messages.js
+++ messages.js
@@ -3,10 +3,16 @@
 
 // @TODO: 3개의 문자열 messages 배열 만들어 주세요.
 let messages;
+messages = ["I'm sleepy","I'm hungry","Hello sleepy, Hello hungry, I'm dad"]
 
 // @TODO: messages 배열에서 각 메시지를 인쇄하는 함수를 만들어 주세요.
 let printMsgs;
-
+printMsgs = () => {
+    for(let i= 0; i < messages.length; i++){
+        console.log(messages[i]);
+    }
+}
+printMsgs();
 
 
 
